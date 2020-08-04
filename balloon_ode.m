3688d73cdb5d9154dd9351887a78c08a2e61d49c
CarolinLennartz <63729648+CarolinLennartz@users.noreply.github.com>
2020-08-04 12:57:38 +0200

    Update balloon_ode.m
    extension of balloon model

--- balloon_ode.m
+++ balloon_ode.m
@@ -21,14 +21,17 @@ E0 = Mu.E0;
 tao0 = Mu.tao0;
 taov = Mu.taov;
 alpha = Mu.alpha;
+tau_s = Mu.tau_s;
+tau_f = Mu.tau_f;
+epsilon= Mu.epsilon;
 
 % 
 if any(tt == t)
     t1=find(t==tt);
-    fin_t = Mu.fin(t1);
+    na = Mu.na(t1);
 else
-    [c,t1]=min(abs(t-tt));
-    fin_t = Mu.fin(t1);
+    [~,t1]=min(abs(t-tt));
+    na = Mu.na(t1);
     
     %Mu.fin(t1),tt(t1),t
 end
@@ -36,11 +39,13 @@ end
 
 q = y(1);
 v = y(2);
-h = y(3);
-dydt = [0;0;0];
+fin_t = y(3);
+s = y(4);
+dydt = [0;0;0;0];
 
 E_t = 1-(1-E0).^(1/fin_t);
 dydt(1) = 1/tao0*(fin_t*E_t/E0-1/(tao0+taov)*(tao0*v.^(1/alpha)+taov*fin_t)*q/v);  %fin_t/tao0 * (E_t/E0 - q/v) + 1/taov * (fin_t - v.^(1/alpha))*q/v;
 dydt(2) = 1/tao0*(fin_t-1/(tao0+taov)*(tao0*v.^(1/alpha)+taov*fin_t));%1/taov * (fin_t - v.^(1/alpha));
-dydt(3) = 1/taov * (fin_t - v.^(1/alpha)*h/v);
+dydt(3) = 1*s;
+dydt(4) = epsilon*na - s/tau_s - (fin_t-1)/tau_f;
 %dydt(3) = fin_t/tao0 * ((1-E_t)/E0 - h/v) + 1/taov * (fin_t - v.^(1/alpha))*h/v;
